dbbb5c551c11938c147c6f060c63b6d1437101b3
Evgeniy Volodin <zheka.az@mail.ru>
2023-10-08 16:36:16 +0300

    Progress!

--- IntersectionOfSegments/AVLTree.h
+++ IntersectionOfSegments/AVLTree.h
@@ -3,29 +3,75 @@
 #define INTERSECTION_OF_SEGMENTS_AVL_TREE_H_
 #include <algorithm>
 #include <iostream>
+#include "Segments.h"
+#include <vector>
 
+template <class T>
 struct Node
 {
-	int key;
+	T key;
 	Node* left, * right;
 	Node* parent;
 	int height;
-	Node(int _key, Node* _parent = nullptr, Node* _left = nullptr, Node* _right = nullptr, int _height = 1) :
+	Node(T _key, Node* _parent = nullptr, Node* _left = nullptr, Node* _right = nullptr, int _height = 1) :
 		key(_key), left(_left), right(_right), parent(_parent), height(_height) {};
 };
 
+template <class T>
 class AVLTree
 {
-	int GetBalance(Node* current);
-	int Height(Node* current) { return current ? current->height : 0; }
-	void FixHeight(Node* current)
+	int GetBalance(Node<T>* current)
+	{
+		return Height(current->right) - Height(current->left);
+	}
+	int Height(Node<T>* current) { return current ? current->height : 0; }
+	void FixHeight(Node<T>* current)
 	{
 		unsigned char height_left = Height(current->left);
 		unsigned char height_right = Height(current->right);
 		current->height = (height_right > height_left ? height_right : height_left) + 1;
 	}
-	void PartInsertRecursive(Node* current, int key);
-	Node* Min(Node* node)
+	void PartInsertRecursive(Node<T>* current, T key)
+	{
+		if (key < current->key) {
+			if (current->left)
+				PartInsertRecursive(current->left, key);
+			else
+				current->left = new Node<T>(key, current);
+			current->left = BalanceNode(current->left);
+		}
+		else {
+			if (current->right)
+				PartInsertRecursive(current->right, key);
+			else
+				current->right = new Node<T>(key, current);
+			current->right = BalanceNode(current->right);
+		}
+	}
+	void PartInsertNodeRecursive(Node<T>* current, Node<T>* keyNode)
+	{
+		if (keyNode->key < current->key) {
+			if (current->left)
+				PartInsertNodeRecursive(current->left, keyNode);
+			else
+			{
+				current->left = keyNode;
+				keyNode->parent = current;
+			}
+			current->left = BalanceNode(current->left);
+		}
+		else {
+			if (current->right)
+				PartInsertNodeRecursive(current->right, keyNode);
+			else
+			{
+				current->right = keyNode;
+				keyNode->parent = current;
+			}
+			current->right = BalanceNode(current->right);
+		}
+	}
+	Node<T>* Min(Node<T>* node)
 	{
 		while (node->left != nullptr)
 		{
@@ -33,7 +79,7 @@ class AVLTree
 		}
 		return node;
 	}
-	Node* Max(Node* node)
+	Node<T>* Max(Node<T>* node)
 	{
 		while (node->right != nullptr)
 		{
@@ -41,19 +87,132 @@ class AVLTree
 		}
 		return node;
 	}
-	Node* root_;
+	Node<T>* root_;
 public:
-	AVLTree(Node* root = nullptr) : root_(root) {};
+	AVLTree(Node<T>* root = nullptr) : root_(root) {};
 
-	Node* RotationRight(Node* current, Node* parent);
-	Node* RotationLeft(Node* current, Node* parent);
-	Node* BalanceNode(Node* current);
+	Node<T>* RotationRight(Node<T>* current, Node<T>* parent)
+	{
+		Node<T>* left = current->left;
+		Node<T>* A = left->left;
+		Node<T>* B = left->right;
+		Node<T>* C = current->right;
+		left->left = A;
+		left->right = current;
+		current->left = B;
+		current->right = C;
+		if (A != nullptr)
+		{
+			A->parent = left;
+		}
+		current->parent = left;
+		if (B != nullptr)
+		{
+			B->parent = current;
+		}
+		if (C != nullptr)
+		{
+			C->parent = current;
+		}
+		left->parent = parent;
+		int th = 0;
+		if (B != nullptr)
+			th = B->height;
+		if (C != nullptr)
+			th = std::max(th, C->height);
+		th++;
+		current->height = th;
+		if (A != nullptr)
+			th = std::max(th, A->height);
+		th++;
+		left->height = th;
+		return left;
+	}
+	Node<T>* RotationLeft(Node<T>* current, Node<T>* parent)
+	{
+		Node<T>* right = current->right;
+		Node<T>* A = right->right;
+		Node<T>* B = right->left;
+		Node<T>* C = current->left;
+		right->right = A;
+		right->left = current;
+		current->right = B;
+		current->left = C;
+		if (A != nullptr)
+		{
+			A->parent = right;
+		}
+		current->parent = right;
+		if (B != nullptr)
+		{
+			B->parent = current;
+		}
+		if (C != nullptr)
+		{
+			C->parent = current;
+		}
+		right->parent = parent;
+		int th = 0;
+		if (B != nullptr)
+			th = B->height;
+		if (C != nullptr)
+			th = std::max(th, C->height);
+		th++;
+		current->height = th;
+		if (A != nullptr)
+			th = std::max(th, A->height);
+		th++;
+		right->height = th;
+		return right;
+	}
+	Node<T>* BalanceNode(Node<T>* current)
+	{
+		FixHeight(current);
+		int b = GetBalance(current);
+		if (b == 2) {
+			if (GetBalance(current->right) < 0)
+				current->right = RotationRight(current->right, current);
+			return RotationLeft(current, current->parent);
+		}
+		else if (b == -2) {
+			if (GetBalance(current->left) > 0)
+				current->left = RotationLeft(current->left, current);
+			return RotationRight(current, current->parent);
+		}
+		return current;
+	}
 
-	Node* Maximum() { return Max(root_); }
-	Node* Minimum() { return Min(root_); }
+	Node<T>* Maximum() { return Max(root_); }
+	Node<T>* Minimum() { return Min(root_); }
 
-	void Insert(const int key);
-	void deleteTree(Node* root)
+	void Insert(const T key)
+	{
+		if (root_ == nullptr) {
+			root_ = new Node<T>(key);
+			return;
+		}
+		PartInsertRecursive(root_, key);
+		root_ = BalanceNode(root_);
+	}
+	void InsertNode(Node<T>* keyNode)
+	{
+		if (root_ == nullptr) {
+			root_ = keyNode;
+			return;
+		}
+		PartInsertNodeRecursive(root_, keyNode);
+		root_ = BalanceNode(root_);
+	}
+
+	void RemoveMin(Node<T>* node) // удаление узла с минимальным ключом из дерева p
+	{
+		if (node->left == 0)
+			return node->right;
+		node->left = RemoveMin(node->left);
+		BalanceNode(node);
+	}
+
+	void deleteTree(Node<T>* root)
 	{
 		if (root != nullptr) {
 			deleteTree(root->left);
@@ -62,10 +221,57 @@ public:
 		}
 	}
 
-	Node* UnderNode(Node* node);
-	Node* OverNode(Node* node);
+	Node<T>* UnderNode(Node<T>* node)
+	{
+		if (node->right != nullptr)
+		{
+			return Min(node->right);
+		}
+		Node<T>* tmp = node->parent;
+		while (tmp != nullptr && node == tmp->right)
+		{
+			node = tmp;
+			tmp = tmp->parent;
+		}
+		return tmp;
+	}
+	Node<T>* OverNode(Node<T>* node)
+	{
+		if (node->left != nullptr)
+		{
+			return Max(node->left);
+		}
+		Node<T>* tmp = node->parent;
+		while (tmp != nullptr && node == tmp->left)
+		{
+			node = tmp;
+			tmp = tmp->parent;
+		}
+		return tmp;
+	}
+	Node<T>& getNode(const T key) {
+		Node<T>* current = root_;
+		while (current != nullptr) {
+			if (key == current->key)
+				return &current;
+			else if (key < current->key)
+				current = current->left;
+			else if (key > current->key)
+				current = current->right;
+		}
+	}
 
-	void OrderedPrint(Node* root) {
+	void OrderedContent(Node<T>* root, T mass[], int& i)//std::vector<T>& mass)
+	{
+		if (root != nullptr) {
+			OrderedContent(root->left, mass, i);
+			mass[i] = root->key;
+			i++;
+			OrderedContent(root->right, mass, i);
+		}
+	}
+
+	void OrderedPrint(Node<T>* root) {
 		if (root != nullptr) {
 			OrderedPrint(root->left);
 			std::cout << root->key << " ";
@@ -73,6 +279,7 @@ public:
 		}
 	}
 	void Print() { OrderedPrint(root_); }
+	void Content(T mass[], int& i) { OrderedContent(root_, mass, i); }
 	~AVLTree() { deleteTree(root_); }
 };
 #endif
--- IntersectionOfSegments/main.cpp
+++ IntersectionOfSegments/main.cpp
@@ -1,19 +1,82 @@
 #include "AVLTree.h"
+#include "Segments.h"
+#include <vector>
+
+const int n = 2;
+
+double Area(Point a, Point b, Point c) {
+	return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+}
+
+bool IntersectProjection(double b1, double e1, double b2, double e2)
+{
+	if (b1 > e1)  std::swap(b1, e1);
+	if (b2 > e2)  std::swap(b2, e2);
+	return std::max(b1, b2) <= std::min(e1, e2);
+}
+
+bool Intersect(const Segment& a, const Segment& b) {
+	return IntersectProjection(a.begin.x, a.end.x, b.begin.x, b.end.x)
+		&& IntersectProjection(a.begin.y, a.end.y, b.begin.y, b.end.y)
+		&& Area(a.begin, a.end, b.begin) * Area(a.begin, a.end, b.end) <= 0
+		&& Area(b.begin, b.end, a.begin) * Area(b.begin, b.end, a.end) <= 0;
+}
+
+bool IntersectionEffective(Segment S[])
+{
+	AVLTree<Segment> L;
+	Point p;
+	AVLTree<Point> begins_ends;
+	for (int i = 0; i < n; ++i)
+	{
+		begins_ends.Insert(S[i].begin);
+		begins_ends.Insert(S[i].end);
+	}
+	Point point[2 * n];
+	int j = 0;
+	begins_ends.Content(point, j); // лексикографическая сортировка 2n концов отрезков
+	Node<Segment>* s;
+	Node<Segment>* s1;
+	Node<Segment>* s2;
+	for (int i = 0; i < 2 * n; ++i)
+	{
+		p = point[i];
+		s = new Node<Segment>(*p.segment);
+		if (p.left == true)
+		{
+			L.InsertNode(s);
+			s1 = L.OverNode(s);
+			s2 = L.UnderNode(s);
+			if ((Intersect(s1->key, s->key)) || (Intersect(s2->key, s->key)))
+			{
+				return true;
+			}
+		}
+		else
+		{
+			L.InsertNode(s);
+			s1 = L.OverNode(s);
+			s2 = L.UnderNode(s);
+			if (Intersect(s1->key, s2->key))
+			{
+				return true;
+			}
+
+		}
+	}
+	return true;
+}
+
+
 
 int main()
 {
-	AVLTree avl;
-	avl.Insert(2);
-	avl.Insert(8);
-	avl.Insert(4);
-	avl.Insert(12);
-	avl.Insert(16);
-	avl.Insert(17);
-	avl.Insert(14);
-	avl.Insert(10);
-	avl.Insert(6);
-	avl.Print();
-	std::cout << "\n";
-	std::cout << avl.UnderNode(avl.Minimum())->key;
+	Point p1(4, 5), p2(2, 4), p3(2, 7), p4(3, 5);
+	Segment segments[2];
+	segments[0].begin = p1;
+	segments[0].end = p2;
+	segments[1].begin = p3;
+	segments[1].end = p4;
+	IntersectionEffective(segments);
 	return 0;
 }
